dcd009592a187961b97e3f79165e17b1ffc0dddb
anoff <offenhaeuser@gmail.com>
2017-04-26 00:25:37 +0200

    :scissors: refactor: move h264 viewer to separate file

--- examples/raspi-cam-stream/index.js
+++ examples/raspi-cam-stream/index.js
@@ -4,18 +4,24 @@ const raspividStream = require('raspivid-stream');
 const app = express();
 const wss = require('express-ws')(app);
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
-
-app.ws('/video-stream', (ws, req) => {
+//app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.use(express.static(__dirname + '/public'));
+app.ws('/', (ws, req) => {
     console.log('Client connected');
 
     ws.send(JSON.stringify({
       action: 'init',
-      width: '960',
-      height: '540'
+      width: '800',
+      height: '600'
     }));
 
-    var videoStream = raspividStream();
+    var videoStream = raspividStream({
+      framerate: 20,
+      width: 800,
+      height: 600,
+      awb: 'fluorescent',
+      rotation: 180
+    });
 
     videoStream.on('data', (data) => {
         ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
--- examples/raspi-cam-stream/readme.md
+++ examples/raspi-cam-stream/readme.md
@@ -6,10 +6,12 @@
 
 Well.. hook up the raspi3 with a camera module, run this code on the raspberry and open [http://raspberrypi:3030](http://raspberrypi:3030) to see your video.
 
+For parameters on the video, modify `index.js` and refer to [raspivid docs](https://github.com/binocarlos/raspivid) for a list of parameters that gets passed downwards.
+
 ## screenshot
 
 ![not much but here's a robot](./screenshot.jpg)
 
 ## credits
 
-This code is heavily inspired by [131/h264-live-player](https://github.com/131/h264-live-player)
+This code is heavily inspired by [131/h264-live-player](https://github.com/131/h264-live-player) and [pi-cam](https://github.com/pimterry/pi-cam).
