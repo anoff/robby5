142c7d5144b2ef2fc230ddb9b1d4cae29048d1e5
Michael Kopp <kopp.michael@yahoo.de>
2017-05-03 21:15:40 +0200

    output ultrasonic reading to ros

--- examples/sensor-to-ros/index.js
+++ examples/sensor-to-ros/index.js
@@ -2,6 +2,9 @@ const Board = require('firmata');
 const Sonar = require('./lib/sonar');
 const Motor = require('./lib/motor');
 const pwait = require('./lib/util').pwait;
+const rosnodejs = require('rosnodejs');
+
+const std_msgs = rosnodejs.require('std_msgs');
 
 const PIN_SONAR = 8;
 const PIN_SERVO = 3;
@@ -10,43 +13,52 @@ let STEP = 20;
 const MIN = 0, MAX = 180;
 let servoPos = MIN;
 
-const sonarBoard = new Board('/dev/ttyACM0', err => {
-  if (err) {
-    throw new Error(err);
-  }
-  console.log('SONAR READY');
-  const sonar = new Sonar(sonarBoard, PIN_SONAR);
-  sonarBoard.servoConfig(PIN_SERVO, 660, 1300);
-  const servoTo = sonarBoard.servoWrite.bind(sonarBoard, PIN_SERVO);
+rosnodejs.initNode('sensor_to_ros', {messages: ['std_msgs/Float32']})
+.then( (nodeHandle) => {
+    distance_publisher = nodeHandle.advertise('/distance', 'std_msgs/Float32');
+    const sonarBoard = new Board('/dev/ttyACM0', err => {
+        if (err) {
+        throw new Error(err);
+        }
+        console.log('SONAR READY');
+        const sonar = new Sonar(sonarBoard, PIN_SONAR);
+        sonarBoard.servoConfig(PIN_SERVO, 660, 1300);
+        const servoTo = sonarBoard.servoWrite.bind(sonarBoard, PIN_SERVO);
 
-  servoTo(servoPos);
+        servoTo(servoPos);
 
-  const nextPos = () => {
-    const tmp = servoPos + STEP;
-    if (tmp > MAX || tmp < MIN) {
-      // change servo direction
-      STEP *= -1;
-    }
-    return servoPos += STEP;
-  };
-  const scan = () => {
-    // execute 4 pings
-    return sonar.multiPing(1)
-    // select the reading with the smallest distance
-    .then(arr => arr.reduce((c, p) => p.value < c.value ? p : c, { value: Infinity }))
-    // add current servo position to the data object
-    .then(data => Object.assign({angle: servoPos}, data))
-    // send new data to web server
-    .then(data => {
-      console.log(`${data.value}(${data.index}) @ ${data.angle}°`);
+        const nextPos = () => {
+        const tmp = servoPos + STEP;
+        if (tmp > MAX || tmp < MIN) {
+        // change servo direction
+        STEP *= -1;
+        }
+        return servoPos += STEP;
+        };
+        const scan = () => {
+        // execute 4 pings
+          return sonar.multiPing(1)
+            // select the reading with the smallest distance
+            .then(arr => arr.reduce((c, p) => p.value < c.value ? p : c, { value: Infinity }))
+            // add current servo position to the data object
+            .then(data => Object.assign({angle: servoPos}, data))
+            // send new data to web server
+            .then(data => {
+                console.log(`${data.value}(${data.index}) @ ${data.angle}°`);
+                // publish the distance to ros
+                msg = new std_msgs.msg.Float32();
+                msg.data = data.value;
+                distance_publisher.publish(msg);
+                })
+          // move to next position
+          .then(() => servoTo(nextPos()))
+            // wait 20ms for servo to move
+            .then(pwait.bind(null, 80))
+            // trigger next scan
+            .then(scan);
+        }
+        // start scanning
+        scan();
     })
-    // move to next position
-    .then(() => servoTo(nextPos()))
-    // wait 20ms for servo to move
-    .then(pwait.bind(null, 80))
-    // trigger next scan
-    .then(scan);
-  }
-  // start scanning
-  scan();
-})
+});
+
